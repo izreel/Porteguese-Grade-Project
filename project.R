488468ffbd8043bd4e267d036f7236dc793da5a4
anhle697 <32230670+anhle697@users.noreply.github.com>
2018-12-04 16:28:23 -0600

    Linear fit

--- project.R
+++ project.R
@@ -36,5 +36,54 @@ text(tree, pretty = 0, cex = 0.7)
 pred_tree = predict(tree, newdata = cleaned_student_por[-training_sets[[1]],])
 mean((pred_tree - cleaned_student_por[-training_sets[[1]], 'G3'])^2)
 
+#converting Mjob to binary attributes
+Student$MTeacherJob = ifelse(Student$Mjob == "teacher", 1, 0)
+Student$MHealthJob = ifelse(Student$Mjob == "health", 1, 0)
+Student$MServiceJob = ifelse(Student$Mjob == "services", 1, 0)
+Student$MHomeJob = ifelse(Student$Mjob == "at_home", 1, 0)
+Student$MOtherJob = ifelse(Student$Mjob == "other", 1, 0)
+
+#converting Fjob to binary attributes
+Student$FTeacherJob = ifelse(Student$Fjob == "teacher", 1, 0)
+Student$FHealthJob = ifelse(Student$Fjob == "health", 1, 0)
+Student$FServiceJob = ifelse(Student$Fjob == "services", 1, 0)
+Student$FHomeJob = ifelse(Student$Fjob == "at_home", 1, 0)
+Student$FOtherJob = ifelse(Student$Fjob == "other", 1, 0)
+
+#converting reason to binary attributes
+Student$RHome = ifelse(Student$reason == "home", 1, 0)
+Student$RReputation = ifelse(Student$reason == "reputation", 1, 0)
+Student$RCourse = ifelse(Student$reason == "course", 1, 0)
+Student$ROther = ifelse(Student$reason == "other", 1, 0)
+
+#converting guardian  to binary attributes
+Student$GMother = ifelse(Student$guardian == "mother", 1, 0)
+Student$GFather = ifelse(Student$guardian == "father", 1, 0)
+Student$GOther = ifelse(Student$guardian == "other", 1, 0)
+
+#converting binary factors
+Student$school = ifelse(Student$school == "MS", 1, 0) #GP = 0, MS = 1
+Student$sex = ifelse(Student$sex == "M", 1, 0) #F = 0, M = 1
+Student$famsize = ifelse(Student$famsize == "GT3", 1, 0) #Less or equal to 3 (LE3) = 0, Greater than 3(GT3) = 1
+Student$address = ifelse(Student$address == "U", 1, 0)  #Rural (R) = 0, Urban (U) = 1
+Student$Pstatus = ifelse(Student$Pstatus == "T", 1, 0) #Alone (A) = 0, Together (T) = 1
+# Yes = 1, No = 0
+Student$schoolsup = ifelse(Student$schoolsup == "yes", 1, 0)
+Student$famsup = ifelse(Student$famsup == "yes", 1, 0)
+Student$activities = ifelse(Student$activities == "yes", 1, 0)
+Student$nursery = ifelse(Student$nursery == "yes", 1, 0)
+Student$higher = ifelse(Student$higher == "yes", 1, 0)
+Student$internet = ifelse(Student$internet == "yes", 1, 0)
+Student$romantic = ifelse(Student$romantic == "yes", 1, 0)
+Student$paid = ifelse(Student$paid == "yes", 1, 0)
 
 #Linear Regression
+attach(Student)
+lm.fit <-lm(G3~school+sex+age+address+famsize+Pstatus+
+            Medu+Fedu+MTeacherJob+MHealthJob+MServiceJob+
+            MHomeJob+MOtherJob+FTeacherJob+FHealthJob+FServiceJob+
+            FHomeJob+FOtherJob+RHome+RCourse+RReputation+ROther+GMother+GFather+ 
+            GOther+traveltime+studytime+
+            failures+schoolsup+famsup+paid+activities+nursery+romantic+famrel+
+            freetime+goout+Dalc+Walc+health+absences, data=Student)
+summary(lm.fit)
